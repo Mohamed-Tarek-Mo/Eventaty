b1363dbbb0100d3775075828b12e5aa3617dd710
Ahmed Mohammed <ahmedelmdfagia@gmail.com>
2025-12-24 23:32:28 +0200

    adding the events page done and editing on the CardComponent

--- components/sections/HomeVenues.tsx
+++ components/sections/HomeVenues.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import HomeVenuesCard from "../HomeVenuesCard";
+import Link from "next/link";
 
 export default function HomeVenues() {
   return (
@@ -13,11 +14,13 @@ export default function HomeVenues() {
           <HomeVenuesCard />
           <HomeVenuesCard />
         </div>
-        <Button className="mx-auto my-10 py-7 flex cursor-pointer">
-          <p className="flex font-normal items-center gap-2 p-6 text-secondary">
-            Explore All Venues <ArrowRight />
-          </p>
-        </Button>
+        <Link href='/venues'>
+          <Button className="mx-auto my-10 py-7 flex cursor-pointer">
+            <p className="flex font-normal items-center gap-2 p-6 text-secondary">
+              Explore All Venues <ArrowRight />
+            </p>
+          </Button>
+        </Link>
       </div>
     </section>
   );
